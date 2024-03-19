f8327cf7b4e468f0ea2ba48ce871c51d0805ae18
01FE22BCA135 <01fe22bca135@kletech.ac.in>
2024-03-19 11:32:38 +0530

    first commit

---
+++ EmployeeDetails.java
@@ -0,0 +1,10 @@
+public class EmployeeDetails {
+    public int emp_id=3;
+    public int salary=10000;
+
+    public void empdetails()
+    {
+        System.out.println("emp_id:"+emp_id);
+        System.out.println("salary:"+salary);
+    }
+}
